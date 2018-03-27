aa4f8f77af99aab2683ef513fedb9d67bd9fbcc7
siem <siemarell@gmail.com>
2018-03-27 17:21:14 +0300

    BinarySearch

--- src/main/scala/BinarySearch.scala
+++ src/main/scala/BinarySearch.scala
@@ -1,3 +1,16 @@
+import scala.annotation.tailrec
+
 object BinarySearch {
-  def find[T](array: Array[Ordered[T]]): Int = ???
+  def search[T](a: Array[T], item: T)(implicit ev$1: T => Ordered[T]): Option[Int] = {
+
+    @tailrec
+    def loop(start: Int, end: Int): Option[Int] = (start + end) >>> 1 match {
+      case _ if start > end => None
+      case mid if a(mid) > item => loop(start, mid - 1)
+      case mid if a(mid) < item => loop(mid + 1, end)
+      case mid => Some(mid)
+    }
+    loop(0, a.length - 1)
+  }
+
 }
--- src/test/scala/BinarySearchTest.scala
+++ src/test/scala/BinarySearchTest.scala
@@ -2,15 +2,38 @@ import org.scalacheck.{Gen, Properties}
 import org.scalatest.{Matchers, PropSpec}
 import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
 
+
 class BinarySearchTest extends PropSpec
   with PropertyChecks
   with GeneratorDrivenPropertyChecks
   with Matchers {
 
-  property("Should correctly find integers"){
-    forAll(Gen.containerOf[Array, Int](Gen[Int]), Gen.Choose(1,100)){ arr =>
-      val sortedArr = arr.sorted
+  val genInt: Gen[Int] = Gen.choose(Int.MinValue, Int.MaxValue)
+  val genSortedIntArray: Gen[Array[Int]] = Gen.containerOf[Array, Int](genInt).map(_.distinct.sorted)
+  val genSortedStringArray: Gen[Array[String]] = Gen.containerOf[Array, String](Gen.alphaStr).map(_.distinct.sorted)
 
+  property("Should correctly find integers"){
+    forAll(genSortedIntArray){ arr =>
+      //val arr = arr1.distinct
+      if (arr.length == 0) assert(BinarySearch.search(arr, 1).isEmpty)
+      else {
+        val randIndex = Gen.choose(0, arr.length - 1).sample.get
+        val randNum = genInt.filter(!arr.contains(_)).sample.get
+        assert(BinarySearch.search(arr, randNum).isEmpty)
+        assert(BinarySearch.search(arr, arr(randIndex)).contains(randIndex))
+      }
+    }
+  }
+
+  property("Should correctly find strings"){
+    forAll(genSortedStringArray){ arr =>
+      if (arr.length == 0) assert(BinarySearch.search(arr, "").isEmpty)
+      else {
+        val randIndex = Gen.choose(0, arr.length - 1).sample.get
+        val randStr = Gen.alphaStr.filter(!arr.contains(_)).sample.get
+        assert(BinarySearch.search(arr,randStr).isEmpty)
+        assert(BinarySearch.search(arr, arr(randIndex)).contains(randIndex))
+      }
     }
   }
 }
